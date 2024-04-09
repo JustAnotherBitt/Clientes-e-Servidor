2492e362141f4fa14cab0d02e860ecaff2f06fa9
Letícia Rodrigues <97632543+LeRodrigues2005@users.noreply.github.com>
2024-04-09 15:45:30 -0300

    Update cliente_tcp.py

--- cliente_tcp.py
+++ cliente_tcp.py
@@ -1,11 +1,11 @@
+# executar somente depois que o servidor tcp estiver em execução
 import socket
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # ipv4 tcp
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # ipv4 TCP
 client.settimeout(1)  # se a conexão não for estabelecida em um segundo, encerra a conexão e retorna except
 
 try:
-    client.connect(("127.0.0.1", 4466))  # ip, porta
-    client.connect(("127.0.0.1", 4422))  # ip, porta
+    client.connect(("127.0.0.1", 4422))  # ip local e porta escolhida
     client.send(b"Sua mensagem aqui\n")  # "b" transforma a string em bytes para poder ser enviada
     pacotes_recebidos = client.recv(1024).decode()
     print(pacotes_recebidos)
@@ -13,4 +13,3 @@ try:
 except Exception as error:
     print("Um erro ocorreu.")
     print(error)
-
