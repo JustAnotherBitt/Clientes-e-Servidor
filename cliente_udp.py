8a7c4384b4401fcbabbdf25c9350167a2900e602
Letícia Rodrigues <97632543+LeRodrigues2005@users.noreply.github.com>
2024-04-09 15:46:01 -0300

    Update cliente_udp.py

--- cliente_udp.py
+++ cliente_udp.py
@@ -1,15 +1,14 @@
 import socket
 
-client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # SOCK_DGRAM porque é udp
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # SOCK_DGRAM porque é UDP
 try:
     while True:
         msg = input("Mensagem: ") + "\n"
-        client.sendto(msg.encode(), ("127.0.0.1", 4433))  # encode() para tranformar a msg em bytes
         client.sendto(msg.encode(), ("127.0.0.1", 4422))  # encode() para tranformar a msg em bytes
 
         data, sender = client.recvfrom(1024)  # 1° elemento da tupla = data; 2° elemento (uma tupla) = sender
         print(sender[0] + ": " + data.decode())
-# sender[0] para pegar apenas o 1° elemento da tupa (que conta como string) porque não pode concatenar tupla com string
+# sender[0] seleciona apenas o 1° elemento da tupa (que conta como string) porque não pode concatenar tupla com string
 
         if data.decode() == "sair\n" or msg == "sair\n":
             break
