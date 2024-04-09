4cebb496b6db303470d91bdd8c72fa5866cec39c
Letícia Rodrigues <97632543+LeRodrigues2005@users.noreply.github.com>
2024-04-09 15:46:27 -0300

    Update servidor.py

--- servidor.py
+++ servidor.py
@@ -1,3 +1,4 @@
+# este é um servidor TCP
 import socket
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -5,14 +6,11 @@ file = open("output.txt", "w")  # cria um arquivo para o output
 
 try:
     server.bind(("0.0.0.0", 4422))
-    server.listen(5)  # escutar em até 5 conexões simultâneas
+    server.listen(5)  # escuta em até 5 conexões simultâneas
     print("Listening...")
 
     client_socket, address = server.accept()  # aceita a conexão (retorna duas tuplas)
     print("Received from: " + address[0])  # pega o primeiro elemento da segunda tupla (colocada na var "address")
-
-    data = client_socket.recv(1024).decode()
-    print(data)
 
     data = client_socket.recv(1024).decode()
 
